929db073dd9e8544fa69140069956c0d554b53fb
LynxN1 <shadowtruddy@gmail.com>
2022-07-31 20:52:55 +0300

    Update templates, add create_task function

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import Tasks
+from .views import Tasks, create_task
 
 urlpatterns = [
     path('', Tasks.as_view(), name='tasks'),
+    path('create/', create_task, name='create_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,33 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.views.generic import ListView, CreateView
+
+from tasks.models import Task
 
 
-class Tasks(LoginRequiredMixin, TemplateView):
+class Tasks(LoginRequiredMixin, ListView):
+    model = Task
     template_name = 'tasks/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = context['object_list'].filter(author=self.request.user)
+        return context
+
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title, author=request.user)
+        return redirect('tasks')
+    if request.method == 'GET':
+        return redirect('tasks')
+
+
+# class CreateTaskView(CreateView):
+#     model = Task
+#     template_name = 'tasks/create_task_modal.html'
+#
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         return super(CreateTaskView, self).form_valid(form)
